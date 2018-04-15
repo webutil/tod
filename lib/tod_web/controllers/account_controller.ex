e7ae70dd05809211ce3d1b0b44a8c323de562c13
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-04-15 15:04:42 +0530

    Add persistence

--- lib/tod_web/controllers/account_controller.ex
+++ lib/tod_web/controllers/account_controller.ex
@@ -1,7 +1,13 @@
 defmodule TODWeb.AccountController do
   use TODWeb, :controller
+  alias TOD.DB
 
   def new(conn, _params) do
-    render conn, :new
+    render(conn, :new)
+  end
+
+  def create(conn, %{"account" => account_params}) do
+    DB.insert_repo(%{name: account_params["github_url"], access_token: "test"})
+    render(conn, :new)
   end
 end
